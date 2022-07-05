e773e0685a30de9437b59d585a540c8c48fceebf
김선재 <codingdeer@gmail.com>
2022-07-05 19:49:28 +0900

    파이보 프로젝트 최초 커밋

---
+++ pybo/views/answer_views.py
@@ -0,0 +1,63 @@
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseNotAllowed
+from django.shortcuts import render, get_object_or_404, redirect, resolve_url
+from django.utils import timezone
+
+from ..forms import AnswerForm
+from ..models import Question, Answer
+
+
+@login_required(login_url="common:login")
+def answer_create(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    if request.method == 'POST':
+        form = AnswerForm(request.POST)
+        if form.is_valid():
+            answer = form.save(commit=False)
+            answer.question = question
+            answer.create_date = timezone.now()
+            answer.author = request.user
+            answer.save()
+            return redirect('{}#answer_{}'.format(resolve_url('pybo:detail', question_id=answer.question.id), answer.id))
+    else:
+        return HttpResponseNotAllowed('Only POST is possible')
+    context = {'question': question, 'form': form}
+    return render(request, 'pybo/question_detail.html', context)
+
+@login_required(login_url='common:login')
+def answer_modify(request, answer_id):
+    answer = get_object_or_404(Answer, pk=answer_id)
+    if request.user != answer.author:
+        messages.error(request, '수정권한이 없습니다')
+        return redirect('pybo:detail', question_id=answer.question.id)
+    if request.method == "POST":
+        form = AnswerForm(request.POST, instance=answer)
+        if form.is_valid():
+            answer = form.save(commit=False)
+            answer.modify_date = timezone.now()
+            answer.save()
+            return redirect('{}#answer_{}'.format(resolve_url('pybo:detail', question_id=answer.question.id), answer.id))
+    else:
+        form = AnswerForm()
+    context = {'answer': answer, 'form': form}
+    return render(request, 'pybo/answer_form.html', context)
+
+@login_required(login_url='common:login')
+def answer_delete(request, answer_id):
+    answer = get_object_or_404(Answer, pk=answer_id)
+    if request.user != answer.author:
+        messages.error('삭제권한이 없습니다.')
+        return redirect('pybo:detail', question_id=answer.question.id)
+    answer.delete()
+    return redirect('pybo:index')
+
+@login_required(login_url='common:login')
+def answer_vote(request, answer_id):
+    answer = get_object_or_404(Answer, pk=answer_id)
+    if request.user == answer.author:
+        messages.error(request, '본인이 작성한 글은 추천할 수 없습니다')
+        return redirect('pybo:detail', question_id=answer.question.id)
+    else:
+        answer.voter.add(request.user)
+    return redirect('{}#answer_{}'.format(resolve_url('pybo:detail', question_id=answer.question.id), answer.id))
